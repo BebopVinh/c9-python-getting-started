a3b8c6d65aeb2999b866b6ff13bfe9807c81eb57
bebopvinh <vinhdo07@gmail.com>
2019-11-16 20:42:30 -0800

    completed numeric variable challenge

--- 5 - Numeric variables/code_challenge.py
+++ 5 - Numeric variables/code_challenge.py
@@ -1,6 +1,9 @@
 # Ask a user to enter a number
+first_num = input('Please enter a number: ')
 # Ask a user to enter a second number
+second_num = input('Please enter another number: ')
 # Calculate the total of the two numbers added together
+total = int(first_num) + int(second_num)
 # Print 'first number + second number = answer' 
-# For example if someone enters 4 and 6 the output should read
-# 4 + 6 = 10
+print(f'{first_num} + {second_num} = {total}')
+
