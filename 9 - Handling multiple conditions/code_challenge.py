78b655c42f68866b3645cc3393a9996c71405bc0
bebopvinh <vinhdo07@gmail.com>
2019-11-17 00:19:48 -0800

    completed multiple conditions challenge

--- 9 - Handling multiple conditions/code_challenge.py
+++ 9 - Handling multiple conditions/code_challenge.py
@@ -12,3 +12,19 @@
 # Charlie should be in room C
 # Khalid Haque should be in room OTHER
 # Xin Zhao should be in room Z
+
+first_name = input ("May I have your first name? ")
+first_letter = first_name[0].lower()
+
+if first_letter in ('a', 'b'):
+   print ("Please go to room AB.")
+elif first_letter == 'c':
+   print ("Please go to room CD.")
+else:
+   last_name = input ("Let me get your last name: ")
+   first_letter = last_name[0].lower()
+   if first_letter == 'z':
+      print ("Please go to room Z.")
+   else:
+      print ("Please go to room OTHER.")
+   
