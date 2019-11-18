d9c4d376363101ae58e1f419b65bec8dfce1fc69
bebopvinh <vinhdo07@gmail.com>
2019-11-17 18:52:50 -0800

    completed complex condition checks challenge

--- 10 - Complex conditon checks/code_challenge.py
+++ 10 - Complex conditon checks/code_challenge.py
@@ -1,8 +1,12 @@
 # When you join a hockey team you get your name on the back of the jersey
 # but the jersey may not be big enough to hold all the letters
 # Ask the user for their first name
+first_name = input("Please give me your first name: ").capitalize()
+first_len = len(first_name)
 
 # Ask the user for their last name
+last_name = input("Please give me your last name: ").capitalize()
+last_len = len(last_name)
 
 # if first name is < 10 characters and last name is < 10 characters 
 #       print first and last name on the jersey
@@ -22,3 +26,16 @@
 # output: R. Ibach
 # first name: ReallyLongFirstName  last name: ReallyLongLastName
 # output: ReallyLongLastName
+
+if last_len < 10:
+   if first_len < 10:
+      print (f'{first_name} {last_name}')
+   else:
+      print (f'{first_name[0]}. {last_name}')
+else:
+   if first_len < 10:
+      print(f'{first_name} {last_name[0]}.')
+   else:
+      print(last_name)
+      
+      
