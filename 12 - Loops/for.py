e8125506f35c74c065ff78343e667f2aac795a56
bebopvinh <vinhdo07@gmail.com>
2019-11-18 01:08:08 -0800

    completed function challenge

--- 12 - Loops/for.py
+++ 12 - Loops/for.py
@@ -1,2 +1,7 @@
-for name in ['Christopher', 'Susan']:
+arr = ['Christopher', 'Susan']
+
+for name in arr:
 	print(name)
+
+for i in range(0, len(arr)):
+	print(i)
--- 13 - Functions/code_challenge.py
+++ 13 - Functions/code_challenge.py
@@ -15,3 +15,17 @@
 # BONUS: Test your function with the values 6, 4 and divide 
 # Have your function return an error message when invalid values are received
 
+def calculator(first_num, second_num, operation):
+   if operation.lower() == 'add':
+      ans = first_num + second_num
+   elif operation.lower() == 'subtract':
+      ans = first_num - second_num
+   else:
+      ans = ("Invalid input.")
+   print (ans)
+      
+      
+calculator(6,4, 'add')
+calculator(6,4, 'subtract')
+calculator(6,4, 'none')
+
